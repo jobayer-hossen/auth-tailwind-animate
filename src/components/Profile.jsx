749bdfadf9ec2c46a63eea531a066c392ad76e94
emonhasan007 <emon45088@gmail.com>
2023-04-28 20:07:53 +0600

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
+        <div className='animation'>
             
+            <div className="flex items-center justify-center">
+        <div className="relative">
+          <div className="  absolute rounded-full top-72 left-4 w-72 h-72 bg-lime-500 filter blur-2xl mix-blend-multiply opacity-75 "></div>
+
+
+          <div className=" absolute rounded-full top-72 -right-4 w-72 h-72 bg-pink-500 filter blur-2xl mix-blend-multiply opacity-75  "></div>
+
+          <div className=" absolute rounded-full top-20 -right-4 w-72 h-72 bg-violet-500 filter blur-2xl  mix-blend-multiply opacity-75  animation-delay-4s"></div>
+
+          <div className=" absolute rounded-full top-20 -left-4 w-72 h-72 bg-amber-500 filter blur-2xl mix-blend-multiply opacity-75 "></div>
+
+        </div>
+      </div>
+
+
         </div>
     );
 };
